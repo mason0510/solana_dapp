825b8f1bec9c961e19edc576e370d8dd7652af11
edddyguo <edddyguo@gmail.com>
2022-08-25 10:40:41 +0800

    +1

--- hello_world/tests/get_all_nft.ts
+++ hello_world/tests/get_all_nft.ts
@@ -8,8 +8,8 @@ import { Connection, clusterApiUrl, Keypair, PublicKey } from "@solana/web3.js";
   const metaplex = new Metaplex(connection);
   metaplex.use(keypairIdentity(keypair));
 
-  const owner = new PublicKey("677NzkzkDKT9wXDMXGPUvbFp1T7XzJtZZxcRaBAaSvNa");
-  const allNFTs = await metaplex.nfts().findAllByOwner(owner).run();
-  console.log(allNFTs);
+  const owner = new PublicKey("6iytHt6hJ9szSvNVL713JoioXPLfoPGjKKTSCUhUtH73");
+  const allNFTs = await metaplex.nfts().findAllByOwner({owner}).run();
+  console.log(JSON.stringify(allNFTs));
 
 })();
--- hello_world/tests/get_avaliable_nft_of_owner.ts
+++ hello_world/tests/get_avaliable_nft_of_owner.ts
@@ -1,6 +1,8 @@
 import { Connection, clusterApiUrl, PublicKey } from '@solana/web3.js';
 import bs58 from 'bs58';
-import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+//import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
+import { CandyMachine } from "@metaplex-foundation/mpl-candy-machine";
+
 
 
 const connection = new Connection(clusterApiUrl('devnet'));
@@ -55,21 +57,24 @@ const getCandyMachineCreator = async (candyMachine: PublicKey): Promise<[PublicK
 );
 
 (async () => {
+
     let collections = [
-        "5vVzukCYp9eDvqTaW4P9NXNco1E82f8ArXCzEo79NTyo",
+        /*"5vVzukCYp9eDvqTaW4P9NXNco1E82f8ArXCzEo79NTyo",
         "3YEWaEFXfLtdC4iEA9ge7Pam52hhNbRb3ZTbF27e4CcC",
         "CPugH8xmFxpXM8898T4cRFT9oYHEUoCnEAiseJP2fb49",
         "DZ9bgaLLZUY6H4iqsSRXR4rJ7fs1HhWTMmpY8bNoCMRh",
+        "AtmjF2VEffyDov5MZmt6XhQLL9SCr8b3baevKEmQAMXG",
         "3XAZ49Ffke8zkGaiEWYVW1sbhV8ZT4XkQyEdpqTGMbZW",
         "Ctj5KL6y4QHaGFjqYXo9j7PwtpzdBe21rMq984i7Qct7",
         "HBQTomn3ryj2odUcWGAfFehMk9erByVTF2uNSYkqRHuz",
-        "HJxHc7Vc4EHWeg1ev3yK8XmXXwqZheXaN7ucQ5xMDqqi",
+        "HJxHc7Vc4EHWeg1ev3yK8XmXXwqZheXaN7ucQ5xMDqqi",*/
         "HYRw4bjkci823ApqoJj9gJzPCwM563XqtLVwoBND8JLL"
     ];
     for (let i=0; i<collections.length; i++)
     {
         console.log("collection_index %d",i);
         const candyMachineCreator = await getCandyMachineCreator(new PublicKey(collections[i]));
+        console.log("candyMachineCreator ",JSON.stringify(candyMachineCreator));
         let nftAddrs = await getMintAddresses(candyMachineCreator[0]);
         for (let i=0; i<nftAddrs.length; i++)
         {
