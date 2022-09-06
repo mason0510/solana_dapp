40aa45bef22525297270fe7d90cf6c4bdf00d72c
edddyguo <edddyguo@gmail.com>
2022-09-06 08:54:19 +0800

    repush nft_marketplace

---
+++ nft_marketplace/escrow_marketplace/programs/escrow_marketplace/src/lib.rs
@@ -0,0 +1,265 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
+use spl_token::instruction::AuthorityType;
+
+declare_id!("Hv49DNdC6CUSwK3MWH5gj5BfLUU64r2ANXwdhaaRceGD");
+
+#[program]
+pub mod escrow_marketplace {
+    use super::*;
+
+    const ESCROW_PDA_SEED: &[u8] = b"escrow";
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        _vault_account_bump: u8, //todo: 暂时无用
+        price: u64,
+    ) -> Result<()> {
+        ctx.accounts.escrow_account.initializer_key = *ctx.accounts.initializer.key;
+        ctx.accounts
+            .escrow_account
+            .seller_token_account = *ctx
+            .accounts
+            .seller_token_account
+            .to_account_info()
+            .key;
+
+        ctx.accounts.escrow_account.price = price;
+
+        let (vault_authority, _vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        token::set_authority(
+            ctx.accounts.into_set_authority_context(),
+            AuthorityType::AccountOwner,
+            Some(vault_authority),
+        )?;
+
+        token::transfer(
+            ctx.accounts.into_transfer_to_pda_context(),
+            1,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
+
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_initializer_context()
+                .with_signer(&[&authority_seeds[..]]),
+            1,
+        )?;
+
+        token::close_account(
+            ctx.accounts
+                .into_close_context()
+                .with_signer(&[&authority_seeds[..]]),
+        )?;
+
+        Ok(())
+    }
+
+    pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        let (_vault_authority, vault_authority_bump) =
+            Pubkey::find_program_address(&[ESCROW_PDA_SEED], ctx.program_id);
+        let authority_seeds = &[&ESCROW_PDA_SEED[..], &[vault_authority_bump]];
+
+        //send K coin to seller from buyer
+        token::transfer(
+            ctx.accounts.into_transfer_to_initializer_context(),
+            ctx.accounts.escrow_account.price,
+        )?;
+
+        //send nft to buyer from vault_account
+        token::transfer(
+            ctx.accounts
+                .into_transfer_to_taker_context()
+                .with_signer(&[&authority_seeds[..]]),
+            1,
+        )?;
+
+        token::close_account(
+            ctx.accounts
+                .into_close_context()
+                .with_signer(&[&authority_seeds[..]]),
+        )?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(vault_account_bump: u8, initializer_amount: u64)]
+pub struct Initialize<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+    pub nft_mint: Account<'info, Mint>,
+    #[account(
+        init,
+        seeds = [b"token-seed8".as_ref()],
+        bump,
+        payer = initializer,
+        token::mint = nft_mint,
+        token::authority = initializer,
+    )]
+    pub vault_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = seller_token_account.amount == 1
+    )]
+    pub seller_token_account: Account<'info, TokenAccount>,
+    #[account(zero)]
+    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub system_program: AccountInfo<'info>,
+    pub rent: Sysvar<'info, Rent>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Cancel<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut, signer)]
+    pub initializer: AccountInfo<'info>,
+    #[account(mut)]
+    pub vault_account: Account<'info, TokenAccount>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub vault_authority: AccountInfo<'info>,
+    #[account(mut)]
+    pub seller_token_account: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint = escrow_account.initializer_key == *initializer.key,
+        constraint = escrow_account.seller_token_account == *seller_token_account.to_account_info().key,
+        close = initializer
+    )]
+    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_program: AccountInfo<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Exchange<'info> {
+    #[account(signer)]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub buyer: AccountInfo<'info>,
+    #[account(mut)]
+    pub buyer_coin_account: Box<Account<'info, TokenAccount>>,  // K coin
+    #[account(mut)]
+    pub buyer_token_account: Box<Account<'info, TokenAccount>>, //nft token
+    #[account(mut)]
+    pub seller_coin_account: Box<Account<'info, TokenAccount>>,
+    #[account(mut)]
+    pub seller_token_account: Box<Account<'info, TokenAccount>>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    #[account(mut)]
+    pub seller: AccountInfo<'info>,
+    #[account(
+        mut,
+        constraint = escrow_account.price <= buyer_coin_account.amount,
+        constraint = escrow_account.seller_token_account == *seller_token_account.to_account_info().key,
+        constraint = escrow_account.initializer_key == *seller.key,
+        close = seller
+    )]
+    pub escrow_account: Box<Account<'info, EscrowAccount>>,
+    #[account(mut)]
+    pub vault_account: Box<Account<'info, TokenAccount>>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub vault_authority: AccountInfo<'info>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    pub token_program: AccountInfo<'info>,
+}
+
+#[account]
+pub struct EscrowAccount {
+    pub initializer_key: Pubkey,
+    pub seller_token_account: Pubkey,
+    pub price: u64, //todo：兼容多币种,可以增加一个token_account 作为接受地址同时约束了币种
+}
+
+impl<'info> Initialize<'info> {
+    fn into_transfer_to_pda_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self
+                .seller_token_account
+                .to_account_info()
+                .clone(),
+            to: self.vault_account.to_account_info().clone(),
+            authority: self.initializer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_set_authority_context(&self) -> CpiContext<'_, '_, '_, 'info, SetAuthority<'info>> {
+        let cpi_accounts = SetAuthority {
+            account_or_mint: self.vault_account.to_account_info().clone(),
+            current_authority: self.initializer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
+impl<'info> Cancel<'info> {
+    fn into_transfer_to_initializer_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.vault_account.to_account_info().clone(),
+            to: self
+                .seller_token_account
+                .to_account_info()
+                .clone(),
+            authority: self.vault_authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
+        let cpi_accounts = CloseAccount {
+            account: self.vault_account.to_account_info().clone(),
+            destination: self.initializer.clone(),
+            authority: self.vault_authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
+
+impl<'info> Exchange<'info> {
+    fn into_transfer_to_initializer_context(
+        &self,
+    ) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.buyer_coin_account.to_account_info().clone(),
+            to: self
+                .seller_coin_account
+                .to_account_info()
+                .clone(),
+            authority: self.buyer.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_transfer_to_taker_context(&self) -> CpiContext<'_, '_, '_, 'info, Transfer<'info>> {
+        let cpi_accounts = Transfer {
+            from: self.vault_account.to_account_info().clone(),
+            to: self.buyer_token_account.to_account_info().clone(),
+            authority: self.vault_authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+
+    fn into_close_context(&self) -> CpiContext<'_, '_, '_, 'info, CloseAccount<'info>> {
+        let cpi_accounts = CloseAccount {
+            account: self.vault_account.to_account_info().clone(),
+            destination: self.seller.clone(),
+            authority: self.vault_authority.clone(),
+        };
+        CpiContext::new(self.token_program.clone(), cpi_accounts)
+    }
+}
---
+++ nft_marketplace/escrow_marketplace/readme2.md
@@ -0,0 +1,10 @@
+
+##mark
+1、vault_account用来存储托管金额
+2、vault_authory是vault_account的权限帐号，同时也是合约的pda帐号
+3、escrow_account是用来存储挂单信息的帐号
+
+##todo
+1、怎么查询遍历所有的挂单，find program ？
+2、兼容多币种
+3、还要判断token_account是否可用，没创建的话在合约里面创建，或者用anchor的init_if_need
