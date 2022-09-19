2934bceaae5793777e0c71fcaad0d665bb75f286
edddyguo <edddyguo@gmail.com>
2022-09-19 11:06:42 +0800

    +1

--- nft_marketplace/escrow_marketplace/programs/escrow_marketplace/src/lib.rs
+++ nft_marketplace/escrow_marketplace/programs/escrow_marketplace/src/lib.rs
@@ -1,37 +1,38 @@
+pub mod buy;
+pub mod cancel;
 pub mod constants;
 pub mod errors;
-pub mod state;
-pub mod buy;
-pub mod sell;
-pub mod cancel;
 pub mod init_settings;
+pub mod sell;
+pub mod state;
 pub mod update_settings;
 
 use anchor_lang::prelude::*;
-use anchor_spl::associated_token;
-use anchor_spl::token::{self, CloseAccount, Mint, SetAuthority, TokenAccount, Transfer};
-use spl_token::instruction::AuthorityType;
-use std::str::FromStr;
 
-use crate::sell::*;
-use crate::cancel::*;
-use crate::update_settings::*;
-use crate::init_settings::*;
-use crate::buy::pay_spl_token::*;
 use crate::buy::pay_lamport::*;
+use crate::buy::pay_spl_token::*;
+use crate::cancel::*;
+use crate::init_settings::*;
+use crate::sell::*;
+use crate::update_settings::*;
 
 declare_id!("D8yTyPU9tSvJc8EuaUqRcvYsAj6SuPoYFg1uZG6istQB");
 
 #[program]
 pub mod escrow_marketplace {
-    use crate::buy::pay_lamport::process_pay_lamport;
-    use crate::buy::pay_spl_token::process_pay_spl_token;
-    use crate::buy::pay_lamport::PayLamport;
-    use crate::buy::pay_spl_token::PaySplToken;
     use super::*;
+    use crate::buy::pay_lamport::process_pay_lamport;
+    use crate::buy::pay_lamport::PayLamport;
+    use crate::buy::pay_spl_token::process_pay_spl_token;
+    use crate::buy::pay_spl_token::PaySplToken;
 
-    pub fn sell(ctx: Context<Sell>, _vault_authority_key: Pubkey, receive_coin: Option<Pubkey>, price: u64) -> Result<()> {
-        process_sell(ctx,receive_coin,price)
+    pub fn sell(
+        ctx: Context<Sell>,
+        _vault_authority_key: Pubkey,
+        receive_coin: Option<Pubkey>,
+        price: u64,
+    ) -> Result<()> {
+        process_sell(ctx, receive_coin, price)
     }
 
     pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
@@ -45,15 +46,19 @@ pub mod escrow_marketplace {
         process_pay_spl_token(ctx)
     }
 
-    pub fn init_settings(ctx: Context<InitSettings>,support_coins: Vec<Pubkey>,fee_ratio: u16) -> Result<()>{
-        process_init_settings(ctx,support_coins,fee_ratio)
+    pub fn init_settings(
+        ctx: Context<InitSettings>,
+        support_coins: Vec<Pubkey>,
+        fee_ratio: u16,
+    ) -> Result<()> {
+        process_init_settings(ctx, support_coins, fee_ratio)
     }
-    pub fn update_settings(ctx: Context<UpdateSettings>,support_coins: Vec<Pubkey>,fee_ratio: u16,new_authority: Option<Pubkey>) -> Result<()>{
-        process_update_settings(ctx,support_coins,fee_ratio,new_authority)
+    pub fn update_settings(
+        ctx: Context<UpdateSettings>,
+        support_coins: Vec<Pubkey>,
+        fee_ratio: u16,
+        new_authority: Option<Pubkey>,
+    ) -> Result<()> {
+        process_update_settings(ctx, support_coins, fee_ratio, new_authority)
     }
 }
-
-
-
-
-
